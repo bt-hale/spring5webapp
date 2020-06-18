3c1a224246c4ff56fa1851b9b9456075993fa832
bthale <billy.hale@northcoast360.com>
2020-06-18 17:42:43 -0500

    First addition
    Pojos and repositories creation

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,65 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import guru.springframework.spring5webapp.domain.Address;
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.EBook;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+	private final AuthorRepository authorRepository;
+	private final BookRepository bookRepository;
+	private final PublisherRepository publisherRepository;
+	
+	
+	public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository,
+			PublisherRepository publisherRepository) {
+		super();
+		this.authorRepository = authorRepository;
+		this.bookRepository = bookRepository;
+		this.publisherRepository = publisherRepository;
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		Author eric = new Author("eric","evans");
+		EBook ddd = new EBook("Domain Driven Designs", "12345");
+		
+		ddd.getAuthors().add(eric);
+		eric.getBooks().add(ddd);
+		
+		authorRepository.save(eric);
+		bookRepository.save(ddd);
+
+		Author rod = new Author("Rod","Johnson");
+		EBook noEJB = new EBook("J2EE Development", "54321");
+		
+		ddd.getAuthors().add(rod);
+		rod.getBooks().add(noEJB);
+		
+		authorRepository.save(rod);
+		bookRepository.save(noEJB);
+
+		System.out.println("Bootstrap Startup");
+		System.out.println("number of books = " + bookRepository.count());
+		
+		Address address = new Address("123 Main", "Dallas", "TX", "75123");
+		Publisher pub = new Publisher("Book Publishers, Inc", address);
+		
+		publisherRepository.save(pub);
+		System.out.println("number of authors = " + authorRepository.count());
+		System.out.println("number of publishers = " + publisherRepository.count());
+		
+	}
+	
+	
+
+}
---
+++ src/main/java/guru/springframework/spring5webapp/domain/EBook.java
@@ -0,0 +1,89 @@
+package guru.springframework.spring5webapp.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class EBook {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	private String title;
+	private String isbn;
+	
+	@ManyToMany
+	@JoinTable(name ="author_book", joinColumns = @JoinColumn(name = "book_id"), 
+									inverseJoinColumns = @JoinColumn(name = "audtor_id"))
+	private Set<Author> authors = new HashSet<>();
+	
+	
+	public EBook() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public EBook(String title, String isbn) {
+		super();
+		this.title = title;
+		this.isbn = isbn;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public Set<Author> getAuthors() {
+		return authors;
+	}
+	public void setAuthors(Set<Author> authors) {
+		this.authors = authors;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EBook other = (EBook) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "EBook [id=" + id + ", title=" + title + ", isbn=" + isbn + ", authors=" + authors + "]";
+	}
+	
+	
+}
